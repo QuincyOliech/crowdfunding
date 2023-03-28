8ab25cf217f3dbf5b3dbd87a49b259f1e61bdd37
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-03-28 14:38:37 +0300

    correcting syntax error && seeding

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_164320) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,48 @@
 require 'faker'
 
-# Set the Faker seed value
-Faker::Config.random = Random.new(1234)
+puts "seeding..................."
 
-#Generate a random start date using Faker
-start_date = Faker::Date.between(from: '2022-01-01', to: '2022-12-31')
-
-# Generate a random end date using Faker between the start date and a year later
-end_date = Faker::Date.between(from: start_date, to: (start_date + 1.year))
-
+# Create 10 users
 10.times do
-User.create(
-    username:Faker::Internet.username,
-    email:Faker::Internet.email
-)
-end
-
+    User.create(
+        username: Faker::Internet.unique.username,
+        email: Faker::Internet.unique.email,
+        password: Faker::Internet.password(min_length: 8)
+    )
+  end
+  
+  # Create 10 campaigns
 10.times do
     Campaign.create(
-        title:Faker::Lorem.sentence(word_count: 3),
-        description:Faker::Lorem.paragraph(sentence_count: 3),
-        category:Faker::Job.field,
-        goal_amount:Faker::Number.decimal(l_digits: 5, r_digits: 2),
-        start_date: start_date,
-        end_date: end_date)
+      title: Faker::Lorem.sentence(word_count: 3),
+      description: Faker::Lorem.paragraph(sentence_count: 3),
+      category: Faker::Job.field,
+      goal_amount: Faker::Number.decimal(l_digits: 5, r_digits: 2),
+      start_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+      end_date: Faker::Date.between(from: 2.months.from_now, to: 1.year.from_now),
+      user_id: Faker::Number.between(from: 1, to: 10)
+    )
+  end
+
+# Create 10 reviews
+10.times do
+Review.create(
+  comment: Faker::Lorem.sentence(word_count: 10),
+  user_id: Faker::Number.between(from: 1, to: 10),
+  campaign_id: Faker::Number.between(from: 1, to: 10),
+  rating: Faker::Number.between(from: 1, to: 5)
+)
+end
+  
+# Create 10 pledges
+10.times do
+  Pledge.create(
+    pledge_amount: Faker::Number.decimal(l_digits: 4, r_digits: 2),
+    user_id: Faker::Number.between(from: 1, to: 10),
+    campaign_id: Faker::Number.between(from: 1, to: 10)
+  )
 end
 
-10.times do
-    Review.create(
-        comment:Faker::Lorem.sentence(word_count: 50),
-        user_id:Faker::Number.between(from: 1, to: 10),
-        campaign_id:Faker::Number.between(from: 1, to: 10)
-    )
-end
 
-10.times do
-    Pledge.create(
-        pledge_amount:Faker::Number.decimal(l_digits: 4, r_digits: 2),
-        user_id:Faker::Number.between(from: 1, to: 10),
-        campaign_id:Faker::Number.between(from: 1, to: 10)
-    )
-end
+puts "Done seeding..................."
+
